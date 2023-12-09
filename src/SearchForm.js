cf514ed2fdc67316c01e523d5234d2f1c5adfaa0
Tohan-B <136444891+Tohan-B@users.noreply.github.com>
2023-12-09 08:51:33 -0800

    Changed class to classname

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -2,27 +2,29 @@ import React from "react";
 
 function SearchForm() {
   return (
-    <form className="search-form" id="search-form">
-      <div class="row">
-        <div class="col-sm-6">
-          <input
-            type="search"
-            placeholder="Type a city.."
-            autofocus="on"
-            autocomplete="off"
-            id="city-input"
-            class="form-control shadow-sm city"
-          />
+    <div>
+      <form className="search-form" id="search-form">
+        <div className="row">
+          <div className="col-sm-6">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              autoFocus="on"
+              autoComplete="off"
+              id="city-input"
+              className="form-control shadow-sm city"
+            />
+          </div>
+          <div className="col-sm-3">
+            <input
+              type="submit"
+              value="Search"
+              className="form-control btn btn-primary shadow-sm button-color"
+            />
+          </div>
         </div>
-        <div class="col-sm-3">
-          <input
-            type="submit"
-            value="Search"
-            class="form-control btn btn-primary shadow-sm button-color"
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
